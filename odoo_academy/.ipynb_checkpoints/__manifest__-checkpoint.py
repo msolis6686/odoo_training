6baec8d3079a5d929fe89a072a65dc7f85eac672
Mario Solis <mariorodrigosolis@hotmail.com>
2021-09-16 18:49:11 +0000

    add

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' :'bf_antanet_customer_cards',
+    'summary' : """Tarjetas de clientes""",
+    'description' : """
+        Tarjetas de créditos o débitos de clientes:
+            - Crear tarjetas de clientes
+            - Impremir reportes de tarjetas 
+    """,
+    'author' : 'BlackFishTeam',
+    'website' : 'https://blackfishweb.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+    'demo' : [
+        
+    ],
+}
---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' :'Odoo Academy',
+    'summary' : """Tarjetas de clientes""",
+    'description' : """
+        Tarjetas de créditos o débitos de clientes:
+            - Crear tarjetas de clientes
+            - Imprimir reportes de tarjetas 
+    """,
+    'author' : 'BlackFishTeam',
+    'website' : 'https://blackfishweb.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+    'demo' : [
+        
+    ],
+}
