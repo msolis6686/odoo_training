f7e1284e4728eaece417aa0ac24c9b47638efe1b
msolis6686 <mariorodrigosolis@hotmail.com>
2021-09-16 02:08:14 +0000

    mod

--- bf_antanet_customer_cards/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ bf_antanet_customer_cards/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import customer_cards
+#from . import customer_cards
---
+++ customercard/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "customercard",
+
+    'summary': """Tarjetas de Clientes...""",
+
+    'description': """
+        Modulo para que se le pueda agregar el departamento y municipio al que pertenece el cliente en la seccion de "Clientes"
+    """,
+
+    'author': "Black Fish",
+    'website': "http://www.blackfisweb.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        
+    ],
+}
