cfde07b36a13b1c325b6aef0aa38bbc0d62ded47
msolis6686 <mariorodrigosolis@hotmail.com>
2021-09-16 01:11:29 +0000

    add

---
+++ bf_antanet_customer_cards/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' :'BF Antanet Customer Cards',
+    'summary' : """Tarjetas de clientes""",
+    'description' : """
+        Tarjetas de créditos o débitos de clientes:
+            - Crear tarjetas de clientes
+            - Impremir reportes de tarjetas 
+    """,
+    'author' : 'BlackFishTeam',
+    'website' : 'https://blackfishweb.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+    'demo' : [
+        
+    ],
+}
