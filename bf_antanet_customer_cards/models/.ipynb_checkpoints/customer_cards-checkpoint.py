474b2ae99cc6a83c9be66e0cfe65f0f68e109a96
msolis6686 <mariorodrigosolis@hotmail.com>
2021-09-16 01:55:35 +0000

    modulo

---
+++ bf_antanet_customer_cards/models/.ipynb_checkpoints/customer_cards-checkpoint.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class bf_antanet_customer_cards(models.Model):
+    _name = 'bf_antanet_customer_cards'
+    _description = 'Tarjetas de clientes'
